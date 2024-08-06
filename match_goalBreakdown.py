0258bea63944228c15ed9ce55ef6ba761cab9e43
francescozonaro <francesco.zonaro@gmail.com>
2024-08-06 19:44:06 +0200

    Remove SVG from goal breakdown + Minor name refactoring

--- match_goalBreakdown.py
+++ match_goalBreakdown.py
@@ -1,8 +1,6 @@
 import socceraction.spadl as spadl
-import xml.etree.ElementTree as ET
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
-from io import BytesIO
 from tabulate import tabulate
 
 from utils import nice_time, config, draw_pitch, get_statsbomb_api
@@ -17,7 +15,7 @@ def find_goal(df):
     return df.index
 
 
-def draw_match_goals(actions):
+def draw_match_goals(actions, with_labels=True):
 
     goals = list(find_goal(actions))
 
@@ -31,12 +29,10 @@ def draw_match_goals(actions):
         cols = ["nice_time", "player_name", "type_name", "result_name", "team_name"]
 
         print(tabulate(df[cols], headers=cols, showindex=True))
-        draw_actions(df, filename="imgs/test_" + str(goal), title="Title")
+        draw_actions(df, "imgs/test_" + str(goal), with_labels)
 
 
-def draw_actions(actions, filename, title=""):
-
-    ET.register_namespace("", "http://www.w3.org/2000/svg")
+def draw_actions(actions, filename, with_labels):
 
     figsize_ratio = config["fig_size"] / 12
     ax = draw_pitch()
@@ -50,6 +46,8 @@ def draw_actions(actions, filename, title=""):
         x_end = action["end_x"]
         y = action["start_y"]
         y_end = action["end_y"]
+
+        team_color = MAIN_COLOR if action["team_name"] == MAIN_TEAM else ALT_COLOR
 
         markersize = 0.8 * figsize_ratio
         linewidth = 1 * figsize_ratio
@@ -74,7 +72,7 @@ def draw_actions(actions, filename, title=""):
                 radius=markersize,
                 edgecolor="black",
                 linewidth=linewidth,
-                facecolor="white",
+                facecolor=team_color,
                 alpha=1,
                 zorder=6,
             )
@@ -111,7 +109,7 @@ def draw_actions(actions, filename, title=""):
                     radius=markersize * 1.2,
                     edgecolor="red",
                     linewidth=linewidth,
-                    facecolor="red",
+                    facecolor=team_color,
                     alpha=0.3,
                     zorder=5,
                 )
@@ -141,7 +139,6 @@ def draw_actions(actions, filename, title=""):
                     + "\n"
                     + action.team_name
                 )
-
         elif action.type_name == "dribble":
             distance = ((x_end - x) ** 2 + (y_end - y) ** 2) ** 0.5
             if actions.iloc[i - 1].type_name != "interception":
@@ -151,7 +148,7 @@ def draw_actions(actions, filename, title=""):
                         radius=markersize,
                         edgecolor="black",
                         linewidth=linewidth,
-                        facecolor="white",
+                        facecolor=team_color,
                         alpha=1,
                         zorder=6,
                     )
@@ -205,15 +202,13 @@ def draw_actions(actions, filename, title=""):
         if isinstance(item, patches.ConnectionPatch):
             x_start, y_start = item.get_path().vertices[0]
             x_end, y_end = item.get_path().vertices[-1]
-
-            # Calculate the center point
             x = (x_start + x_end) / 2
             y = (y_start + y_end) / 2
         else:
             x = item.center[0]
             y = item.center[1]
 
-            if item.get_facecolor() != (1.0, 0.0, 0.0, 0.3):
+            if item.radius != 1.2:
                 ax.text(
                     x,
                     y - 0.1,
@@ -226,37 +221,81 @@ def draw_actions(actions, filename, title=""):
                 )
                 count += 1
 
-        annotate = ax.annotate(
-            label,
-            xy=(x, y),
-            xytext=(10, 10),
-            textcoords="offset points",
-            color="w",
-            ha="left",
-            fontsize=fontsize,
-            zorder=8,
-            bbox=dict(
-                boxstyle="round, pad=0.5",
-                fc=(0.1, 0.1, 0.1, 0.92),
-                ec=(1.0, 1.0, 1.0),
-                lw=1,
-            ),
-        )
+        if with_labels:
+            if isinstance(item, patches.Circle):
+                if "ball received" not in label:
+                    annotate = ax.annotate(
+                        label,
+                        xy=(x, y),
+                        xytext=(10, 10),
+                        textcoords="offset points",
+                        color="w",
+                        ha="left",
+                        fontsize=fontsize,
+                        zorder=8,
+                        bbox=dict(
+                            boxstyle="round, pad=0.5",
+                            fc=(0.1, 0.1, 0.1, 0.92),
+                            ec=(1.0, 1.0, 1.0),
+                            lw=1,
+                        ),
+                    )
 
-        extra_height = 0.2  # Adjust this value as needed
-        bbox = annotate.get_bbox_patch()
-        bbox.set_boxstyle("round,pad=" + str(extra_height))
+                    extra_height = 0.2  # Adjust this value as needed
+                    bbox = annotate.get_bbox_patch()
+                    bbox.set_boxstyle("round,pad=" + str(extra_height))
 
-        # Add text inside the circle
         ax.add_patch(patch)
-        patch.set_gid(f"mypatch_{i:03d}")
-        annotate.set_gid(f"mytooltip_{i:03d}")
+
+    legend_elements = [
+        plt.scatter(
+            [],
+            [],
+            s=130,
+            edgecolor="black",
+            linewidth=linewidth,
+            facecolor=MAIN_COLOR,
+            zorder=5,
+            marker="o",
+            label=MAIN_TEAM,
+        ),
+        plt.scatter(
+            [],
+            [],
+            s=130,
+            edgecolor="black",
+            linewidth=linewidth,
+            facecolor=ALT_COLOR,
+            zorder=5,
+            marker="o",
+            label=ALT_TEAM,
+        ),
+    ]
+
+    ax.legend(
+        handles=legend_elements,
+        loc="upper center",
+        ncol=len(legend_elements),
+        bbox_to_anchor=(0.5, 0.99),
+        fontsize=10,
+        fancybox=True,
+        frameon=True,
+        handletextpad=0.5,
+    )
 
     goal_row = actions.iloc[len(actions) - 1]
     ax.text(
         1,
         66,
-        f"{goal_row['player_name'] +  ' (' + goal_row['nice_time'] + ')'}",
+        f"{goal_row['player_name']}",
+        fontsize=10,
+        va="center",
+        ha="left",
+    )
+    ax.text(
+        1,
+        64,
+        f"{goal_row['nice_time']}",
         fontsize=10,
         va="center",
         ha="left",
@@ -272,56 +311,7 @@ def draw_actions(actions, filename, title=""):
         ha="left",
     )
 
-    f = BytesIO()
-    plt.savefig(f, format="svg", bbox_inches="tight")
-
-    # --- Add interactivity ---
-
-    # Create XML tree from the SVG file.
-    tree, xmlid = ET.XMLID(f.getvalue())
-    tree.set("onload", "init(event)")
-
-    for i in shapes:
-        # Get the index of the shape
-        index = shapes.index(i)
-        # Hide the tooltips
-        tooltip = xmlid[f"mytooltip_{index:03d}"]
-        tooltip.set("visibility", "hidden")
-        # Assign onmouseover and onmouseout callbacks to patches.
-        mypatch = xmlid[f"mypatch_{index:03d}"]
-        mypatch.set("onmouseover", "ShowTooltip(this)")
-        mypatch.set("onmouseout", "HideTooltip(this)")
-
-    # This is the script defining the ShowTooltip and HideTooltip functions.
-    script = """
-        <script type="text/ecmascript">
-        <![CDATA[
-
-        function init(event) {
-            if ( window.svgDocument == null ) {
-                svgDocument = event.target.ownerDocument;
-                }
-            }
-
-        function ShowTooltip(obj) {
-            var cur = obj.id.split("_")[1];
-            var tip = svgDocument.getElementById('mytooltip_' + cur);
-            tip.setAttribute('visibility', "visible")
-            }
-
-        function HideTooltip(obj) {
-            var cur = obj.id.split("_")[1];
-            var tip = svgDocument.getElementById('mytooltip_' + cur);
-            tip.setAttribute('visibility', "hidden")
-            }
-
-        ]]>
-        </script>
-        """
-
-    # Insert the script at the top of the file and save it.
-    tree.insert(0, ET.XML(script))
-    ET.ElementTree(tree).write(f"{filename}.svg")
+    plt.savefig(f"{filename}.png", format="png", bbox_inches="tight", dpi=300)
 
 
 api = get_statsbomb_api()
@@ -344,4 +334,9 @@ df_actions = df_actions.sort_values(
     by=["period_id", "time_seconds"], ascending=[True, True]
 ).reset_index(drop=True)
 
-draw_match_goals(df_actions)
+MAIN_COLOR = "#bf616a"
+ALT_COLOR = "#81a1c1"
+MAIN_TEAM = teams[1]
+ALT_TEAM = teams[0]
+
+draw_match_goals(df_actions, with_labels=True)
