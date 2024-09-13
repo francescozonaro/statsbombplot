b6734b8ec05c0b3f8eb9eba7f65bb2121d6a75ee
francescozonaro <francesco.zonaro@gmail.com>
2024-09-13 11:51:19 +0200

    restarting

--- match/__init__.py
+++
@@ -1,7 +0,0 @@
-from .matchBase import BaseMatchSBP
-from .eventScatterplot import ScatterplotSBP
-from .eventProgressive import ProgressiveSBP
-from .shotMap import ShotmapSBP
-from .shotFreezed import ShotfreezedSBP
-from .goalBreakdown import GoalBreakdownSBP
-from .passingNetwork import PassingNetworkSBP
--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .common import change_range, get_statsbomb_api, adjust_color, derive_near_colors
-from .pitch import Pitch
-from .loader import Loader
